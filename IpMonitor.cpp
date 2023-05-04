e671ee4e37c041abaac2031ac48d4adc17639336
Chandra Sekhar Temmanaboina <37570335+chandu-tmba@users.noreply.github.com>
2023-05-04 17:54:10 +0530

    Update IpMonitor.cpp

--- IpMonitor.cpp
+++ IpMonitor.cpp
@@ -31,21 +31,25 @@
 #define DBGPRINT(X)  /* */
 #endif
 
-
+constexpr auto MAPPER_BUSNAME = "xyz.openbmc_project.ObjectMapper";
+constexpr auto MAPPER_OBJ_PATH = "/xyz/openbmc_project/object_mapper";
+constexpr auto MAPPER_IFACE = "xyz.openbmc_project.ObjectMapper";
+constexpr auto DBUS_PROPERTY_IFACE = "org.freedesktop.DBus.Properties";
 
 using namespace std;
+using PropertyValue = std::variant<uint8_t, uint16_t, std::string,
+                                   std::vector<std::string>, bool>;
 
 std::shared_ptr<sdbusplus::asio::connection> systemBus;
 std::string dhcpobjectpath;
 
-int onPropertyUpdate(sd_bus_message* m, void* /* userdata */,
-
-                            sd_bus_error* retError)
+// call back handler for property changed signal
+int onPropertyUpdate(sd_bus_message* m, void* /* userdata */, sd_bus_error* retError)
 {
-    DBGPRINT("PropertyUpdated  \n");	
+
+    DBGPRINT("PropertyUpdated  \n");
     if (retError == nullptr || sd_bus_error_is_set(retError))
     {
-        //BMCWEB_LOG_ERROR << "Got sdbus error on match";
 	DBGPRINT("Got sdbus error on match \n");
         return 0;
     }
@@ -59,7 +63,6 @@ int onPropertyUpdate(sd_bus_message* m, void* /* userdata */,
     auto it = changedProperties.find("Address");
 
     if (it == changedProperties.end())
-
     {
         return 0;
     }
@@ -74,12 +77,10 @@ int onPropertyUpdate(sd_bus_message* m, void* /* userdata */,
     DBGPRINT("#####New IP: \n");
     DBGPRINT( *newip);
     DBGPRINT("#############\n");
-
-
     return 0;
 }
 
-
+// Regsiter property changed signal 
 void registerDCHPMonSignal()
 {
 
@@ -113,7 +114,6 @@ void registerDCHPMonSignal()
 	  "arg0namespace='xyz.openbmc_project.Network.DHCPConfiguration',"
 	  "member='PropertiesChanged'");
 
-
 	//cerating matches for each interface 
 	auto matchold = std::make_unique<sdbusplus::bus::match::match>(*systemBus,propertiesMatchString1,onPropertyUpdate,nullptr);
 	//ading to matches vector
@@ -123,103 +123,156 @@ void registerDCHPMonSignal()
 	matches.emplace_back(std::move(matchnew));*/
 
 	DHCPIPMonitor = std::make_unique<sdbusplus::bus::match::match>(*systemBus, propertiesMatchString, onPropertyUpdate,nullptr);
-
 	return;
 }
 
-
-
-void get_dhcp_object_paths()
+// Get service name
+const std::string getService(const std::string& path,
+                                          const std::string& interface)
 {
+    std::unordered_map<std::string, std::vector<std::string>> mapperResponse;
+    using InterfaceList = std::vector<std::string>;
 
-	constexpr const std::array<const char*, 1> NetworkIP = {"xyz.openbmc_project.Network.IP"};
+    auto mapper = systemBus->new_method_call(MAPPER_BUSNAME, MAPPER_OBJ_PATH,
+                                      MAPPER_IFACE, "GetObject");
 
+    mapper.append(path, InterfaceList({interface}));
+
+    auto mapperResponseMsg = systemBus->call(mapper);
+    mapperResponseMsg.read(mapperResponse);
+    if (mapperResponse.empty())
+    {
+	std::cerr << "empty response fo get service" << "\n";    
+        return "";
+    }
+
+    // the value here will be the service name
+    return mapperResponse.cbegin()->first;
+}
+
+// Get the property name
+const PropertyValue getProperty(const std::string& objectPath,
+                             const std::string& interface,
+                             const std::string& propertyName)
+{
+    PropertyValue value{};
+
+    auto service = getService(objectPath, interface);
+    if (service.empty())
+    {
+        return value;
+    }
+
+    auto method = systemBus->new_method_call(service.c_str(), objectPath.c_str(),
+                                      DBUS_PROPERTY_IFACE, "Get");
+    method.append(interface, propertyName);
+
+    auto reply = systemBus->call(method);
+    reply.read(value);
+
+    return value;
+}
+
+void get_dhcp_async_paths_async()
+{
+        constexpr const std::array<const char*, 1> NetworkIP = {"xyz.openbmc_project.Network.IP"};
 	systemBus->async_method_call(
 			[](const boost::system::error_code ec,
 				const std::vector<std::string>& objects) {
-			DBGPRINT("(((((((SUBTREE  PATHS CALLBACK BEGIN )))))))\n");
 
+			DBGPRINT("(((((((SUBTREE  PATHS CALLBACK BEGIN )))))))\n");
 
 			if (ec)
 			{
-				DBGPRINT("error in GetSubtreePaths function \n");
+			DBGPRINT("error in GetSubtreePaths function \n");
 
 			}
 
 			for (const std::string& objectpath : objects)
 			{
-			         DBGPRINT("########LOOP BEGIN########\n");
-			         DBGPRINT("!!!!!!!!object_path: \n");
-				 DBGPRINT(objectpath);
-			 	 DBGPRINT("!!!!!!!!!!!!!!!!!!!!!\n");	 
-
-			         systemBus->async_method_call(
+			systemBus->async_method_call(
 					[objectpath](const boost::system::error_code ec2,
 						const std::variant<std::string>& origin) 
 					{
 					DBGPRINT("&&&&&&&&CALLBACK BEGIN &&&&&&&\n");
-
 					int err = 0;
 					if (ec2)
 					{
 					DBGPRINT("$$$$$$$ ERROR IN CALLBACK $$$$$$$\n");
 					err = 1;	
 					}
-
-			                if(err !=1 )
+					if(err !=1 )
 					{
-				       
 					const std::string* addrtype = std::get_if<std::string>(&origin);
-
-					if(*addrtype == "xyz.openbmc_project.Network.IP.AddressOrigin.DHCP")
+					if(*addrtype == "xyz.openbmc_project.Network.IP.AddressOrigin.Static")
 					{
 					//register for this object
-					DBGPRINT("*****DHCP PBJECT FOUND ******\n");
-					DBGPRINT(objectpath);
-
-				        dhcpobjectpath = objectpath;
-
-					DBGPRINT("*****************************\n");
-
+						dhcpobjectpath = objectpath;
 					}
-
-
 					}
 					DBGPRINT("&&&&&&& CALLBACK END &&&&&&&&&&\n");
 					}
-					,
+			        ,
 				"xyz.openbmc_project.Network",
 				objectpath,
 				"org.freedesktop.DBus.Properties", "Get",
 				"xyz.openbmc_project.Network.IP", "Origin");
-				
-				DBGPRINT("########LOOP END########\n");		
 
-				}
+			DBGPRINT("########LOOP END########\n");		
+			}
 			DBGPRINT("(((((((SUBTREE  PATHS CALLBACK END )))))))\n");
 			},
 	  "xyz.openbmc_project.ObjectMapper",
 	  "/xyz/openbmc_project/object_mapper",
           "xyz.openbmc_project.ObjectMapper", "GetSubTreePaths",
-	  "/xyz/openbmc_project/network", 0,
+	  "/", 0,
 	  NetworkIP );
 
+    sleep(120);	
+}	
+
+void get_dhcp_object_paths()
+{
+
+    DBGPRINT(" get_dhcp_object_paths called \n");
+    std::string interface = "xyz.openbmc_project.Network.IP";
+    std::string objectPath = "/";
+    std::string propName = "Origin";	
+    std::vector<std::string> paths;
+
+    auto method = systemBus->new_method_call(MAPPER_BUSNAME, MAPPER_OBJ_PATH,
+                                      MAPPER_IFACE, "GetSubTreePaths");
+    method.append(objectPath.c_str());
+    method.append(0); // Depth 0 to search all
+    method.append(std::vector<std::string>({interface.c_str()}));
+    auto reply = systemBus->call(method);
+
+    reply.read(paths);
+
+    for(auto path: paths)
+    {	    
+	    std::cout << "path:" << path << "\n";
+	    auto val = getProperty(path,interface, propName);
+	    auto origin = std::get<std::string>(val);
+	    if( origin == "xyz.openbmc_project.Network.IP.AddressOrigin.Static")
+            {
+		    std::cout << "static ip found " << "\n";
+		    dhcpobjectpath = path;
+	    }			    
+    }
 }
 
 
 int main()
 {
-        //boost::asio::io_service io;
-	auto io = std::make_shared<boost::asio::io_context>();
-        systemBus = std::make_shared<sdbusplus::asio::connection>(*io);
-
-	//det dhcp objects
+        boost::asio::io_context io;
+	systemBus = std::make_shared<sdbusplus::asio::connection>(io);
+	
+	// Get dhcp objects paths
 	get_dhcp_object_paths();
-	sleep(20);
 
-        //registering for property change
+        //register call back for property change
         registerDCHPMonSignal();
 	
-	io->run();
-
+	io.run();
 }
